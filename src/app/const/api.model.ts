c2ae813321b02d53b10648031560aec6e0be5cdb
victor bravo <ahal_tocob@hotmail.com>
2022-03-17 19:30:41 -0600

    Ajustes varios

--- src/app/const/api.model.ts
+++ src/app/const/api.model.ts
@@ -1,5 +1,6 @@
 export class Api{
-   public static  urlBase : string = "https://localhost:44361/api";
+   public static  urlBase : string = "https://expenses.silly-lovelace.198-38-83-250.plesk.page/api";
+   //public static  urlBase : string = "https://localhost:44361/api";
    //public static  urlBase : string = "http://localhost/Expenses/api";
 
    public static urlApiEntries : string = Api.urlBase + "/Entries/"
